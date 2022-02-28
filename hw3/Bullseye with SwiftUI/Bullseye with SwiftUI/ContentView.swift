035334e704767ad1b65c1ccf42cfd77dbf807df5
Coco Ma <coccoo.ma@berkeley.edu>
2022-02-28 14:13:15 -0800

    final submission

---
+++ hw3/Bullseye with SwiftUI/Bullseye with SwiftUI/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  Bullseye with SwiftUI
+//
+//  Created by Tony Hong on 2/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var num: Double = 50
+    @State var toggle = false
+    var body: some View {
+        
+        VStack() {
+            HStack() {
+                VStack() {
+                    Text("High Score")
+                    Text("0")
+                }
+                .padding(.leading, 20)
+                .padding(.top, 20)
+                
+                Spacer()
+                
+                VStack() {
+                    Text("Current Level")
+                    Text("1")
+                }
+                .padding(.trailing, 20)
+                .padding(.top, 20)
+            }
+            Spacer()
+            
+            Text("Move the slider to:").font(.system(size: 30))
+            Text("25").font(.system(size: 30))
+            Slider(value: $num, in: 0...100).padding(.leading).padding(.trailing)
+            Button("Check") {
+            
+            }
+            Spacer()
+            
+            Text("Exact Mode?")
+            Toggle(isOn: $toggle) {
+        
+            }.padding(.trailing, 165)
+            
+        }
+        
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
